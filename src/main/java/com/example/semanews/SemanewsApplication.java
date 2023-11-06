1006455ab789adeb322612d5bb7ee3d8bb9e4f67
Raiga SASAYAMA <raiga0310brilio@gmail.com>
2023-11-06 14:04:45 +0900

    initial commit

---
+++ src/main/java/com/example/semanews/SemanewsApplication.java
@@ -0,0 +1,13 @@
+package com.example.semanews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemanewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/semanews/SemanewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.semanews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemanewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
