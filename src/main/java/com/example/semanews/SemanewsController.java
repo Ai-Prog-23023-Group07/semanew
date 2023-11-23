0434343b9971ad5d7596056b5d08771cafe83956
Maxi Ohashi <cmt14019@nitech.jp>
2023-11-23 21:12:39 +0900

    リポジトリを分けた

--- src/main/java/com/example/semanews/SemanewsController.java
+++ src/main/java/com/example/semanews/SemanewsController.java
@@ -11,25 +11,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+
 @Controller
 @RequestMapping("/genres")
 public class SemanewsController {
 
-    private final SemanewsRepository semanewsRepository;
+    private final GenreRepository genreRepository;
+    private final TopicRepository topicRepository;
+    private final ArticleRepository articleRepository;
 
     @Autowired
-    public SemanewsController(SemanewsRepository semanewsRepository) {
-        this.semanewsRepository = semanewsRepository;
+    public SemanewsController(GenreRepository genreRepository, TopicRepository topicRepository, ArticleRepository articleRepository) {
+        this.genreRepository = genreRepository;
+        this.topicRepository = topicRepository;
+        this.articleRepository = articleRepository;
     }
-
+    
     @RequestMapping("/main")
     private String showGenre(Model model){
-        List<GenreEntity> genreEntities = semanewsRepository.findGenre();
-        List<TopicEntity> topicEntities = semanewsRepository.findTopicByGenre(genreEntities.get(0).getName());
+        List<GenreEntity> genreEntities = genreRepository.findGenre();
+        List<TopicEntity> topicEntities = topicRepository.findTopic();
+        List<ArticleEntity> articleEntities = articleRepository.findArticle();
 
         System.out.println(topicEntities);
+        System.out.println(articleEntities);
 
-        //testはジャンル名のみを取り出したString型のリスト
         List<String> genres = new ArrayList<String>();
         for(int i=0; i<genreEntities.size();i++){
             genres.add(genreEntities.get(i).getName());
@@ -38,8 +44,14 @@ public class SemanewsController {
         for(int i=0; i<topicEntities.size();i++){
             topics.add(topicEntities.get(i).getName());
         }
+        List<String> articles = new ArrayList<String>();
+        for(int i=0; i<articleEntities.size();i++){
+            articles.add(articleEntities.get(i).getName());
+        }
+
         model.addAttribute("genres", genres);
         model.addAttribute("topics", topics);
+        model.addAttribute("articles", articles);
 
         return "/semNewsTest.html";
     }
