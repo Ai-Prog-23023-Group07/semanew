d608bab6a5df21de28edea0edc3e8c67412c29ab
Maxi Ohashi <cmt14019@nitech.jp>
2023-11-23 19:35:58 +0900

    ジャンルを指定するとトピックを3つ返すメソッドを作成 返ってこない

--- src/main/java/com/example/semanews/SemanewsRepository.java
+++ src/main/java/com/example/semanews/SemanewsRepository.java
@@ -1,6 +1,7 @@
 package com.example.semanews;
 
 import org.springframework.data.neo4j.repository.query.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 
 import java.util.List;
@@ -9,4 +10,7 @@ public interface SemanewsRepository extends Neo4jRepository<GenreEntity, Long> {
 
     @Query("MATCH (g:Genre) RETURN g Limit 4")
     List<GenreEntity> findGenre();
+
+    @Query("MATCH (t:Topic)<-[:content]-(g:Genre{name: $name}) RETURN t Limit 3")
+    List<TopicEntity> findTopicByGenre(@Param("name")String name);
 }
