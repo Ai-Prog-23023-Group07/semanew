ecc26c9769e0c9f5666f4b864e79d4001d85a017
Maxi Ohashi <cmt14019@nitech.jp>
2023-11-24 19:14:56 +0900

    screen1の表示

--- src/main/java/com/example/semanews/SemanewsController.java
+++ src/main/java/com/example/semanews/SemanewsController.java
@@ -6,14 +6,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
 @Controller
-@RequestMapping("/genres")
 public class SemanewsController {
 
     private final GenreRepository genreRepository;
@@ -27,39 +30,18 @@ public class SemanewsController {
         this.articleRepository = articleRepository;
     }
     
-    @RequestMapping("/main")
-    private String showGenre(Model model){
+    @RequestMapping("/screen1")
+    private String showscreen1(Model model){
         List<GenreEntity> genreEntities = genreRepository.findGenre();
-        List<TopicEntity> topicEntities = topicRepository.findTopic();
-        List<ArticleEntity> articleEntities = articleRepository.findArticle();
-
-        System.out.println(topicEntities);
-        System.out.println(articleEntities);
-
-        List<TopicEntity> topicEntitiesByGenre = topicRepository.findTopicByGenre("政治");
-        List<ArticleEntity> articleEntitiesByTopic = articleRepository.findArticleByTopic("選挙");
-
-        System.out.println(topicEntitiesByGenre);
-        System.out.println(articleEntitiesByTopic);
         
         List<String> genres = new ArrayList<String>();
         for(int i=0; i<genreEntities.size();i++){
             genres.add(genreEntities.get(i).getName());
         }
-        List<String> topics = new ArrayList<String>();
-        for(int i=0; i<topicEntities.size();i++){
-            topics.add(topicEntities.get(i).getName());
-        }
-        List<String> articles = new ArrayList<String>();
-        for(int i=0; i<articleEntities.size();i++){
-            articles.add(articleEntities.get(i).getTitle());
-        }
 
         model.addAttribute("genres", genres);
-        model.addAttribute("topics", topics);
-        model.addAttribute("articles", articles);
 
-        return "/semNewsTest.html";
+        return "/screen1th.html";
     }
 }
 
